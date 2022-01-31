a790590bed9382cb29d171643a1d8d4d58c0bcc7
Igor Araujo <igoraujo93@gmail.com>
2022-01-31 07:33:20 -0300

    account login teste

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Aisoftware.Tracker.Admin.Models;
+
+namespace Aisoftware.Tracker.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Validate(Admins admin)
+        {
+            
+            var _admin = new Admins()
+            {
+                Email = "teste@teste.com",
+                Password = "123"
+            };
+
+            if(_admin.Email == admin.Email)
+            {
+                if(_admin.Password == admin.Password)
+                {
+                    return Json(new { status = true, message = "Login Successfull!"});
+                }
+                else
+                {
+                    return Json(new { status = false, message = "Invalid Password!"});
+                }
+            }
+            else
+            {
+                return Json(new { status = false, message = "Invalid Email!"});
+            }
+        }
+    }
+
+}
