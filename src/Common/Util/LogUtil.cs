7ba4c9561d6a900a89a920693aef79b6f0829b0f
Igor Araujo <igoraujo93@gmail.com>
2022-03-18 11:23:05 -0300

    add logutil

---
+++ src/Common/Util/LogUtil.cs
@@ -0,0 +1,18 @@
+using System;
+using Aisoftware.Tracker.Admin;
+
+namespace Aisoftware.Tracker.Admin.Common.Util
+{
+    public static class LogUtil
+    {
+        public static string Succes(string className, string ActionName)
+        {
+            return $"SUCCESS: { className }::{ ActionName }";
+        }
+
+        public static string Error(string className, string ActionName, Exception exception)
+        {
+            return $"ERROR: { className }::{ ActionName }\nEXCEPTION:{ExceptionHelper.InnerException(exception).Message}";
+        }
+    }
+}
