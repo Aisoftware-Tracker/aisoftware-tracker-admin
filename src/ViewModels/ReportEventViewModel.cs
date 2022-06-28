ec44da8e0d174cff15338210ae2ebee04e3c91ed
Igor Araujo <igoraujo93@gmail.com>
2022-06-27 22:37:50 -0300

    fix: corrigindo null pointers do relatorio de eventos

--- src/ViewModels/ReportEventViewModel.cs
+++ src/ViewModels/ReportEventViewModel.cs
@@ -5,15 +5,15 @@ namespace Aisoftware.Tracker.Admin.Models
     public class ReportEventViewModel
     {
         public ReportEvent Event { get; set; }
-        public IEnumerable<ReportEvent> Events { get; set; }
+        public IEnumerable<ReportEvent>? Events { get; set; }
         public Device Device { get; set; }
-        public IEnumerable<Device> Devices { get; set; }
+        public IEnumerable<Device>? Devices { get; set; }
         public Position Position { get; set; }
-        public IEnumerable<Position> Positions { get; set; }
+        public IEnumerable<Position>? Positions { get; set; }
         public Geoference Geoference { get; set; }
-        public IEnumerable<Geoference> Geoferences { get; set; }
+        public IEnumerable<Geoference>? Geoferences { get; set; }
         public Maintenance Maintenance { get; set; }
-        public IEnumerable<Maintenance> Maintenances { get; set; }
+        public IEnumerable<Maintenance>? Maintenances { get; set; }
 
     }
 }
